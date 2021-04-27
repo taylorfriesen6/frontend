9001bf3454b4a56da5187672434c9c55a301a5aa
Zachary Cooremans <zcooremans@gmail.com>
2021-04-27 17:08:51 -0500

    Done some styling to change it up

--- my-app/src/components/IndividualPlant.js
+++ my-app/src/components/IndividualPlant.js
@@ -1,28 +1,37 @@
 import React from 'react'
+import '../App.css'
+import styled from 'styled-components'
+
+const Card = styled.div `
+width: 270px;
+height: 550px;
+box-shadow: 0px 30px 60px -40px rgba(130, 70, 0, 0.5);
+
+`
 
 const IndividualPlant = (props) => {
     const {plant} = props
 
     return (
-        <div>
+        <Card>
             <div className='imgContainer'>
                 <img src={plant.plant_image} alt='plant'/>
             </div>
 
             <div className='cardInfo'>
                 <h2>{plant.plant_nickname}</h2>
-                <p>{plant.plant.scientific_name}</p>
+                <p>{plant.plant_scientific_name}</p>
 
-                <p>Watering Schedule</p>
+                <h3>Watering Schedule</h3>
                 <p>{plant.water_schedule} {plant.water_day}</p>
 
-                <p>Location</p>
+                <h3>Location</h3>
                 <p>{plant.plant_location}</p>
 
-                <p>Notes</p>
+                <h3>Notes</h3>
                 <p>{plant.notes}</p>
             </div>
-        </div>
+        </Card>
     )
 }
 export default IndividualPlant
