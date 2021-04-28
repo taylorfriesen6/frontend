e7d655ae9dd6d95b1201a0aaf3aef1b006b5d92f
ClintFix <clint.fix@gmail.com>
2021-04-27 21:02:14 -0600

    built SpeciesCard component

--- my-app/src/components/SpeciesCard.js
+++ my-app/src/components/SpeciesCard.js
@@ -6,17 +6,26 @@ import styled from 'styled-components';
 const CardContainer = styled.div`
     width: 270px;
     height: 411px;
+    margin-bottom: 40px;
+    box-shadow: 0px 30px 60px -40px rgba(130, 70, 0, 0.5);
 `
 const PlantImage = styled.img`
     width: 270px;
     height: 270px;
     object-fit: cover;
 `
+const PlantDetails = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px 15px 10px;
+`
 const PlantName = styled.p`
     font-size: 18px;
     font-family: serif; //TODO: change to PT Serif
     font-weight: bold;
     color: 224229;
+    margin-top: 15px;
 `
 const PlantSpecies = styled.p`
     font-size: 13px;
@@ -24,22 +33,30 @@ const PlantSpecies = styled.p`
     font-weight: 600;
     font-style: italic;
     color: #BCB6A6;
+    margin-top: 10px;
+    margin-bottom: 15px;
+    border-bottom: 3px solid #CBAD91;
+    width: 100%;
 `
-const SpeciesUnderline = styled.div`
-    border-top: 3px solid #CBAD91;
+const WaterSchedule = styled.p`
+    color: #224229;
+    font-size: 15px;
+    font-family: serif; //TODO: change to PT Serif
+    margin-top: 3px;
 `
 
 const SpeciesCard = (props) => {
-    const { plant_image, plant_name, plant_scientific_name, species_id, water_schedule } = props.plant;
+    console.log(props.plant)
+    const { plant_image, plant_name, plant_scientific_name, water_schedule } = props.plant;
     return (
         <CardContainer>
             <PlantImage src={plant_image} alt={plant_name} />
-            <div>
+            <PlantDetails>
                 <PlantName>{plant_name}</PlantName>
                 <PlantSpecies>{plant_scientific_name}</PlantSpecies>
-                <SpeciesUnderline/>
                 <h3>Watering Schedule</h3>
-            </div>
+                <WaterSchedule>{water_schedule}</WaterSchedule>
+            </PlantDetails>
         </CardContainer>
     )
 }
