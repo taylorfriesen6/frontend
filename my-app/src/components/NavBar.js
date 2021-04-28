8aace58e0238bfa608140f91cdc06368f1a61af9
ClintFix <clint.fix@gmail.com>
2021-04-28 09:20:13 -0600

    fixed store

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from "styled-components";
 import { NavLink, useHistory } from 'react-router-dom'
-import { connect } from "react-redux";
 
 
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { store } from "./store/Store";
+
 
 
 
