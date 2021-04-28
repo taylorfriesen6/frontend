e37935389f6b6ce1a2a1c3893819849ed1aae3fc
nickcabuno <nickcabuno17@gmail.com>
2021-04-28 14:36:04 -0400

    ,

--- my-app/src/store/Store.js
+++ my-app/src/store/Store.js
@@ -6,5 +6,5 @@ import thunk from 'redux-thunk';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(
   rootReducer,
-  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+  composeEnhancers(applyMiddleware(thunk))
 );
