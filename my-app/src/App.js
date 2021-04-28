d97229ed203ffa68143063892fa4b9b5e106643c
ClintFix <clint.fix@gmail.com>
2021-04-27 20:05:40 -0600

    species list component created. Route added (addPlant)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import PlantCollection from './components/PlantCollection'
+import PlantCollection from './components/PlantCollection';
+import SpeciesList from './components/SpeciesList';
 import  NavBar  from "./components/NavBar";
 import { Route } from "react-router-dom";
 
@@ -12,7 +13,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      
+      <Route path="/addplant" component={SpeciesList} />
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
---
+++ my-app/src/components/SpeciesList.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import styled from 'styled-components';
+
+const SpeciesList = () => {
+
+    return (
+        <div>
+            <div>
+                <h2>Add A Plant</h2>
+                <p>Cancel X</p>
+            </div>
+        </div>
+    )
+};
+
+export default SpeciesList;
