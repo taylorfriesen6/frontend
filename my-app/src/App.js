997c29322d356dcbff142d4b25727acb01eee606
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-04-28 21:19:21 -0600

    merge conflict

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,18 +15,13 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Route path='/collection' component={PlantCollection} />
-      <Route path="/no-plants" component={NoPlants} />
-      <Route path="/individual-plant" component={IndividualPlant} />
-      <Route path="/addplant" component={SpeciesList} />
-      <Route exact path="/" component={Home} />
-      <Route path="/addplant" component={SpeciesList} />
-      <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
-      <Route path='/collection' component={PlantCollection} />
-      <Route path="/no-plants" component={NoPlants} />
-      <Route path="/individual-plant" component={IndividualPlant} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute path='/collection' component={PlantCollection} />
+      <ProtectedRoute path="/addplant" component={SpeciesList} />
+      <ProtectedRoute path="/no-plants" component={NoPlants} />
+      <ProtectedRoute path="/individual-plant" component={IndividualPlant} />
     </div>
   );
 }
