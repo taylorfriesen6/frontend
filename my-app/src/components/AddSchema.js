85419768d3ff4fc58caa8729fd49ab05979a863b
Zachary Cooremans <zcooremans@gmail.com>
2021-04-29 12:30:52 -0500

    TAKE IT

--- my-app/src/components/AddSchema.js
+++ my-app/src/components/AddSchema.js
@@ -4,7 +4,6 @@ const addSchema = yup.object().shape({
     plant_nickname: yup.string().required('Plant name is required'),
     plant_location: yup.string().required(' Location is required'),
     water_day: yup.string().oneOf(['1',"2","3","4","5","6","7"],"required"),
-    notes: yup.string().required('required'),
 })
 
 export default addSchema
